f116bb779d3f5523ba1e1ae25b455d04af5009e5
Chethan Kumar <Chethan Kumar@LAPTOP-0VMGE6A8>
2025-06-27 13:29:23 +0530

    Second commit

--- Shivakumar/src/test/java/org/TestDemo_Automation/Demo_Test_Automatiom_1.java
+++ Shivakumar/src/test/java/org/TestDemo_Automation/Demo_Test_Automatiom_1.java
@@ -1,4 +1,4 @@
-package org.TestDemo;
+package org.TestDemo_Automation;
 
 import java.util.concurrent.TimeUnit;
 
@@ -7,31 +7,29 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
-public class Demo1 {
-
+public class Demo_Test_Automatiom_1 {
+	
 	static WebDriver driver;
-	// TODO Auto-generated method stub
-@Test(priority = 1)
-	public static void login() throws InterruptedException 
+	@Test
+	// Login
+	public static void Login() throws InterruptedException
 	{
-		System.setProperty("webdriver.chrome.driver", "D:\\eclipse_latest\\Shivakumar\\Drivers\\chromedriver.exe");
-		 driver=new ChromeDriver();
-		//WebDriver driver=new ChromeDriver();	
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Chethan Kumar\\eclipse-workspace\\Automation\\Drivers\\chromedriver.exe");
+		 driver=new ChromeDriver();	
         driver.get("https://online.actitime.com/synamedia/timetrack/enter.do");
       //  https://online.actitime.com/synamedia/timetrack/enter.do
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
-        Thread.sleep(3000);
         driver.findElement(By.id("username")).sendKeys("chethankumarklk23@gmail.com");
         Thread.sleep(3000);
         driver.findElement(By.xpath("//input[@class='textField pwdfield']")).sendKeys("CHET1996@");
         Thread.sleep(3000);
         driver.findElement(By.id("loginButton")).click();
-        Thread.sleep(3000);
 	}
         
-	@Test(priority = 2)
-	public static void create_customer() throws InterruptedException 
+	@Test	 
+		// Create Customer
+	public static void create_customer() throws InterruptedException
 	{
 		// WebDriver driver=null;
 		  driver.findElement(By.xpath("//div[text()='Tasks']")).click();
@@ -47,8 +45,9 @@ public class Demo1 {
 		  driver.findElement(By.xpath("//div[text()='Create Customer']")).click();
 			Thread.sleep(1000);  
 	}
-@Test(priority = 3)
-	public static void delete_customer() throws InterruptedException 
+	@Test
+	// Delete Customer
+	public static void delete_customer() throws InterruptedException
 	{
 		//WebDriver driver=null; 
 
@@ -61,5 +60,6 @@ public class Demo1 {
 		  driver.findElement(By.xpath("//*[@class='blockedWarningContainer']//following-sibling::div[@class='buttonsContainer']//child::div[@class='submitBtn commitBtn greyButton']//span[text()='Delete permanently']")).click();
 		 Thread.sleep(2000);
 		 driver.close();
-	}
+        } 
+
 }
